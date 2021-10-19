b693559b17eed22922bc27ea138f3bffe9fc4971
JohnM-Kahura <Johnmungai392@gmail.com>
2021-10-19 12:01:09 +0300

    add hive db

--- lib/screens/home/qr/qr.dart
+++ lib/screens/home/qr/qr.dart
@@ -1,4 +1,3 @@
-import 'package:baratonchurch/utils/database.dart';
 import 'package:baratonchurch/utils/student.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
