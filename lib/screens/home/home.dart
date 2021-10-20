ab7ca0803874f5ee0d1a8653782b74149be30324
JohnM-Kahura <Johnmungai392@gmail.com>
2021-10-21 02:45:02 +0300

    home view coded

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:baratonchurch/screens/home/notificationApi.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,12 +10,129 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isReminder = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('home'),
-      ),
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 3),
+              height: MediaQuery.of(context).size.width - 120,
+              width: double.infinity,
+              color: Colors.red,
+              child: Stack(
+                children: [
+                  Image(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.width - 120,
+                      fit: BoxFit.fill,
+                      image: const AssetImage('images/background.jpg')),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Know Where you are going ',
+                        style: GoogleFonts.oxygen(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          Text(
+                            'By Dr. Rei Kesis',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Monday Service',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              NotificationsApi.showNotification(
+                                  title: 'Baraton Church',
+                                  body: 'fuck the government',
+                                  payload: 'church stuff');
+                              print('object');
+                              // setState(() {
+                              //   isReminder = !isReminder;
+                              // });
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    isReminder
+                                        ? Icons.notifications_active
+                                        : Icons.notifications,
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Row(
+                            children: const [
+                              Icon(
+                                Icons.event,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'Monday',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Text(
+                            '5:00-6:00pm',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                          const Text(
+                            '12/10/2021',
+                            style: TextStyle(
+                              color: Colors.white,
+                              // fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
